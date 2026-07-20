64304d1843aa0b36f55e3cb8ff59d6f70543a48a
MellyASD <melissaramirezortegon@hotmail.com>
2026-07-20 18:24:32 -0500

    [FEATURE] Hamburguesa para movil

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,32 @@
+import { useState } from "react";
+import "../styles/navbar.css";
+
+
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="navbar">
-
       <div className="logo">
         <h1>The Sims</h1>
       </div>
 
-      <nav className="menu">
+      <div
+        className={`menu-toggle ${menuOpen ? "active" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </div>
+
+      <nav className={`menu ${menuOpen ? "active" : ""}`}>
         <ul>
           <li><a href="#">Inicio</a></li>
           <li><a href="#">Catálogo</a></li>
           <li><a href="#">Expansiones</a></li>
           <li><a href="#">Comunidad</a></li>
         </ul>
+        <button className="btn-login">Iniciar sesión</button>
       </nav>
-
-      <button className="btn-login">
-        Iniciar sesión
-      </button>
-
     </header>
   );
 }
