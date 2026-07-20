7f2178b5becfe363991a3b55dda84a2312318b03
MellyASD <melissaramirezortegon@hotmail.com>
2026-07-20 18:11:40 -0500

    [UPDATE] Hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,23 +1,18 @@
 function Hero() {
   return (
     <section className="hero">
-
       <div className="hero-info">
         <h2>Expande tu mundo</h2>
-
-        <p>
-          Descubre los últimos packs de expansión para PC y Móvil.
-        </p>
-
-        <button>
-          Ver catálogo
-        </button>
+        <p>Descubre los últimos packs de expansión para PC y Móvil.</p>
+        <button>Ver catálogo</button>
       </div>
 
       <div className="hero-image">
-        Imagen Hero
+        <img 
+          src="https://res.cloudinary.com/w1jl4sa5/image/upload/v1784588991/TS4_Royalty-and-Legacy_Sale-Hero_16x9-ES_mmadik.avif" 
+          alt="Imagen Hero" 
+        />
       </div>
-
     </section>
   );
 }
